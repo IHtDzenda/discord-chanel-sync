c53d3918e4b8dda8edefe23b825acd48ef0f10e4
dzenda <111608778+IHtDzenda@users.noreply.github.com>
2023-02-28 16:50:29 +0100

    cleared up

--- testing.js
+++
@@ -1,6 +0,0 @@
-var items=[
-    { firstID: '1079429715640729650', SeconID: '1079429745365758024' },
-    { firstID: '1079505195135942739', SeconID: '1079505225188122736' }
-]
-    console.log(items[0].firstID)
-
