3bae4a4f98615502d222cd43b4dec2021ec194fd
IhtDzenda <nev27im@gmail.com>
2023-03-01 19:20:44 +0100

    added command for linking chanels

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const BOT_TOKEN = ""
+const BOT_TOKEN = ""			//<=======Token here 
 const fs = require('fs');
 const csv = require('csv-parser');
 const csvPath=('./chanel-config.csv');
 let botName;
+const prefix = '!';
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -17,12 +19,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+	if(!message.content.startsWith(prefix)||message.author.bot){
 	console.log(`${message.author.tag} said: ${message.content}`);
 	if(message.author.tag!=botName){
 		get_Linked_ChanellID(message.channel.id,message.content,message.author.tag)
-	}
+		
+	}}
 	
+	const args = message.content.slice(prefix.length).split(/ -/);
+	console.log(args);
 
+	if (args[0]=="linkChanells"&&args.length==3){
+		create_Chanell_Config(args[1],args[2])
+	}
 
   });
 
@@ -30,7 +39,19 @@ client.on('channelCreate', channel => {
   console.log(`${channel.name} has been created in ${channel.guild.name}`);
 });
 function create_Chanell_Config(sourceID,DestinationID){
+	//let data="firstID,secondID,";
+	fs.readFile(csvPath, 'utf8', function(err, data){
+		console.log("Read: "+data);
+		data=data+"\n"+sourceID+","+DestinationID;
+		fs.writeFile(csvPath, data, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("File sucesfully edited");
+		}); 
+	});
 
+	
 
 }
 function get_Linked_ChanellID(ID,message,author){
