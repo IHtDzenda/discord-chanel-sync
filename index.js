6fe0ff90c5b2766ca17b8b685b718864185a4f71
IhtDzenda <nev27im@gmail.com>
2023-03-02 12:38:15 +0100

    removd promises fixed some missing packages

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const BOT_TOKEN = ""			//<=======Token here 
 const fs = require('fs');
-const csv = require('csv-parser');
+const { parse } = require('csv-parse');
 const csvPath=('./chanel-config.csv');
 let botName;
 const prefix = '!';
 
 
-client.on('ready', () => {
+client.on('ready', async ()  => {
   console.log(`Logged in as ${client.user.tag}!`);
   botName=client.user.tag;
-  readCsvFile()
-  .then((results) => {
-	console.log("Current config: ")
-	console.log(results)
-	})
+  console.log(await readCsvFile())
+  
+
+	
 });
 
 client.on('message', message => {
@@ -23,17 +22,18 @@ client.on('message', message => {
 	console.log(`${message.author.tag} said: ${message.content}`);
 	if(message.author.tag!=botName){
 		get_Linked_ChanellID(message.channel.id,message.content,message.author.tag)
-		
-	}}
+		}
+		return;
+	}
 	
 	const args = message.content.slice(prefix.length).split(/ -/);
-	console.log(args);
-
 	if (args[0]=="linkChanells"&&args.length==3){
 		create_Chanell_Config(args[1],args[2])
 	}
 
+
   });
+  
 
 client.on('channelCreate', channel => {
   console.log(`${channel.name} has been created in ${channel.guild.name}`);
@@ -54,34 +54,43 @@ function create_Chanell_Config(sourceID,DestinationID){
 	
 
 }
-function get_Linked_ChanellID(ID,message,author){
-	readCsvFile()
-	.then((results) => {
+async function get_Linked_ChanellID(ID,message,author){
+	const results = await readCsvFile();
+
 		for (let index = 0; index < results.length; index++) {
 			if (results[index].firstID==ID) {
 				console.log("Sended message :"+message+" as user "+author+"   ["+results[index].secondID+"]")
-				client.channels.cache.get(results[index].secondID).send(author+": "+message)
+				client.channels.cache.get(results[index].secondID).send(author+": "+message)		
 			}
 		}
-	})
 }
 
-function readCsvFile() {
-	const results = [];
+
+async function readCsvFile() {
+	try {
+	const fileContents = await fs.promises.readFile(csvPath);
+    const parser = parse(fileContents, { columns: true });
+	const records = [];
   
-	return new Promise((resolve, reject) => {
-	  fs.createReadStream(csvPath)
-		.pipe(csv())
-		.on('data', (data) => {
-		  results.push(data);
-		})
-		.on('end', () => {
-		  resolve(results);
-		})
-		.on('error', (error) => {
-		  reject(error);
+	parser.on('readable', () => {
+		let record;
+		while ((record = parser.read())) {
+		records.push(record);
+		}
+	});
+	await new Promise((resolve, reject) => {
+		parser.on('end', () => {
+		resolve();
+		});
+		parser.on('error', (error) => {
+			reject(error);
 		});
 	});
+	return records;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
   }
   
   
